436cf96db379dee662cd37207dddb055af98348c
Matthew D. Steele <mdsteele@alum.mit.edu>
2016-05-09 12:31:47 -0400

    Add example binary for reading ICNS files

---
+++ examples/readicns.rs
@@ -0,0 +1,22 @@
+extern crate icns;
+
+use icns::IconFamily;
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+
+fn main() {
+    if env::args().count() != 2 {
+        println!("Usage: readicns <path>");
+        return;
+    }
+    let path = env::args().nth(1).unwrap();
+    let file = File::open(path).expect("failed to open file");
+    let buffered = BufReader::new(file);
+    let family = IconFamily::read(buffered).expect("failed to read ICNS file");
+    println!("ICNS file contains {} element(s).", family.elements().len());
+    for (index, element) in family.elements().iter().enumerate() {
+        println!("Element {}: {} ({} byte payload)",
+                 index, element.ostype(), element.data().len());
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@
 extern crate byteorder;
 
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
+use std::fmt;
 use std::io::{self, Read, Write};
 
 /// The first four bytes of an ICNS file:
@@ -34,7 +35,19 @@ pub struct IconElement {
 
 /// A Macintosh OSType (also known as a ResType), used in ICNS files to
 /// identify the type of each icon element.
-pub type OSType = [u8; 4];
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct OSType(pub [u8; 4]);
+
+impl fmt::Display for OSType {
+    fn fmt(&self, out: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        let &OSType(raw) = self;
+        for &byte in &raw {
+            let character = std::char::from_u32(u32::from(byte)).unwrap();
+            try!(write!(out, "{}", character));
+        }
+        Ok(())
+    }
+}
 
 impl IconFamily {
     /// Creates a new, empty icon family.
@@ -118,8 +131,8 @@ impl IconElement {
 
     /// Reads an icon element from an ICNS file (or other reader).
     fn read<R: Read>(mut reader: R) -> io::Result<IconElement> {
-        let mut ostype = [0u8; 4];
-        try!(reader.read_exact(&mut ostype));
+        let mut raw_ostype = [0u8; 4];
+        try!(reader.read_exact(&mut raw_ostype));
         let element_length = try!(reader.read_u32::<BigEndian>());
         if element_length < ICON_ELEMENT_HEADER_LENGTH {
             return read_error("invalid element length");
@@ -127,12 +140,13 @@ impl IconElement {
         let data_length = element_length - ICON_ELEMENT_HEADER_LENGTH;
         let mut data = vec![0u8; data_length as usize];
         try!(reader.read_exact(&mut data));
-        Ok(IconElement::new(ostype, data))
+        Ok(IconElement::new(OSType(raw_ostype), data))
     }
 
     /// Writes the icon element into an ICNS file (or other writer).
     fn write<W: Write>(&self, mut writer: W) -> io::Result<()> {
-        try!(writer.write_all(&self.ostype));
+        let OSType(ref raw_ostype) = self.ostype;
+        try!(writer.write_all(raw_ostype));
         try!(writer.write_u32::<BigEndian>(self.length()));
         try!(writer.write_all(&self.data));
         Ok(())
@@ -163,17 +177,18 @@ mod tests {
             Cursor::new(b"icns\0\0\0\x1fquux\0\0\0\x0efoobarbaz!\0\0\0\x09#");
         let family = IconFamily::read(input).expect("read failed");
         assert_eq!(2, family.elements().len());
-        assert_eq!(*b"quux", family.elements()[0].ostype());
+        assert_eq!(OSType(*b"quux"), family.elements()[0].ostype());
         assert_eq!(6, family.elements()[0].data().len());
-        assert_eq!(*b"baz!", family.elements()[1].ostype());
+        assert_eq!(OSType(*b"baz!"), family.elements()[1].ostype());
         assert_eq!(1, family.elements()[1].data().len());
     }
 
     #[test]
     fn write_icon_family_with_fake_elements() {
         let mut family = IconFamily::new();
-        family.add_element(IconElement::new(*b"quux", b"foobar".to_vec()));
-        family.add_element(IconElement::new(*b"baz!", b"#".to_vec()));
+        family.add_element(IconElement::new(OSType(*b"quux"),
+                                            b"foobar".to_vec()));
+        family.add_element(IconElement::new(OSType(*b"baz!"), b"#".to_vec()));
         let mut output: Vec<u8> = vec![];
         family.write(&mut output).expect("write failed");
         assert_eq!(b"icns\0\0\0\x1fquux\0\0\0\x0efoobarbaz!\0\0\0\x09#",
