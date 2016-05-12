9651e5e4ee9045758b36bca3fa7e93c330e23950
Matthew D. Steele <mdsteele@alum.mit.edu>
2016-05-11 23:36:51 -0400

    Add ability to specify OSType for png2icns

--- examples/png2icns.rs
+++ examples/png2icns.rs
@@ -9,29 +9,52 @@
 //!
 //! Note that the dimensions of the input image must be exactly those of one of
 //! the supported icon types (for example, 32x32 or 128x128).
+//!
+//! To create an ICNS file from a PNG using a specific icon type within the
+//! ICNS file, run:
+//!
+//! ```shell
+//! cargo run --example png2icns <path/to/file.png> <ostype>
+//! # ICNS will be saved to path/to/file.<ostype>.icns
+//! ```
+//!
+//! Where <ostype> is the OSType for the icon type you want to encode in.  In
+//! this case, the dimensions of the input image must match the particular
+//! chosen icon type.
 
 extern crate icns;
 
-use icns::{IconFamily, Image};
+use icns::{IconFamily, IconType, Image, OSType};
 use std::env;
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
 use std::path::Path;
+use std::str::FromStr;
 
 fn main() {
-    if env::args().count() != 2 {
-        println!("Usage: png2icns <path>");
+    let num_args = env::args().count();
+    if num_args < 2 || num_args > 3 {
+        println!("Usage: png2icns <path> [<ostype>]");
         return;
     }
     let png_path = env::args().nth(1).unwrap();
     let png_path = Path::new(&png_path);
     let png_file = BufReader::new(File::open(png_path)
-                                       .expect("failed to open PNG file"));
+                                      .expect("failed to open PNG file"));
     let image = Image::read_png(png_file).expect("failed to read PNG file");
     let mut family = IconFamily::new();
-    family.add_icon(&image).expect("failed to encode image");
-    let icns_path = png_path.with_extension("icns");
+    let icns_path = if num_args == 3 {
+        let ostype = OSType::from_str(&env::args().nth(2).unwrap()).unwrap();
+        let icon_type = IconType::from_ostype(ostype)
+                            .expect("unsupported ostype");
+        family.add_icon_with_type(&image, icon_type)
+              .expect("failed to encode image");
+        png_path.with_extension(format!("{}.icns", ostype))
+    } else {
+        family.add_icon(&image).expect("failed to encode image");
+        png_path.with_extension("icns")
+    };
     let icns_file = BufWriter::new(File::create(icns_path)
-                                      .expect("failed to create ICNS file"));
+                                       .expect("failed to create ICNS file"));
     family.write(icns_file).expect("failed to write ICNS file");
 }
--- tests/golden.rs
+++ tests/golden.rs
@@ -21,6 +21,23 @@ fn encode_it32() {
     assert_families_match(&family, &reference);
 }
 
+#[test]
+fn decode_ic07() {
+    let family = load_icns_file("ic07.icns").unwrap();
+    let image = family.get_icon_with_type(IconType::RGBA32_128x128).unwrap();
+    let reference = load_png_file("128x128.png").unwrap();
+    assert_images_match(&image, &reference);
+}
+
+#[test]
+fn encode_ic07() {
+    let image = load_png_file("128x128.png").unwrap();
+    let mut family = IconFamily::new();
+    family.add_icon_with_type(&image, IconType::RGBA32_128x128).unwrap();
+    let reference = load_icns_file("ic07.icns").unwrap();
+    assert_families_match(&family, &reference);
+}
+
 fn load_icns_file(name: &str) -> io::Result<IconFamily> {
     let path = format!("tests/icns/{}", name);
     let file = BufReader::new(try!(File::open(path)));
