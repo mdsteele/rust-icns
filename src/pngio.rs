0cd51bc8d0c3d41e6acf292e74e4b8924965a3e1
Kornel <kornel@geekhood.net>
2018-04-17 00:55:05 +0100

    Replace try!() with ?

--- src/pngio.rs
+++ src/pngio.rs
@@ -8,7 +8,7 @@ impl Image {
     /// Reads an image from a PNG file.
     pub fn read_png<R: Read>(input: R) -> io::Result<Image> {
         let decoder = png::Decoder::new(input);
-        let (info, mut reader) = try!(decoder.read_info());
+        let (info, mut reader) = decoder.read_info()?;
         let pixel_format = match info.color_type {
             png::ColorType::RGBA => PixelFormat::RGBA,
             png::ColorType::RGB => PixelFormat::RGB,
@@ -32,7 +32,7 @@ impl Image {
         }
         let mut image = Image::new(pixel_format, info.width, info.height);
         assert_eq!(image.data().len(), info.buffer_size());
-        try!(reader.next_frame(image.data_mut()));
+        reader.next_frame(image.data_mut())?;
         Ok(image)
     }
 
@@ -50,7 +50,7 @@ impl Image {
         };
         let mut encoder = png::Encoder::new(output, self.width, self.height);
         encoder.set(color_type).set(png::BitDepth::Eight);
-        let mut writer = try!(encoder.write_header());
+        let mut writer = encoder.write_header()?;
         writer.write_image_data(&self.data).map_err(|err| match err {
             png::EncodingError::IoError(err) => err,
             png::EncodingError::Format(msg) => {
