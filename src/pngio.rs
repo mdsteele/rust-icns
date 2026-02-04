153f3c189659803869c597de965edef830e6df90
M Stoeckl <code@mstoeckl.com>
2026-02-03 19:25:47 -0500

    Update png library version
    This requires regenerating PNG images in tests checking exact
    output, because the default compression has changed slightly.

--- src/pngio.rs
+++ src/pngio.rs
@@ -1,61 +1,54 @@
-use png;
-use std::io::{self, Read, Write};
 use image::{Image, PixelFormat};
+use png;
+use std::io::{self, BufRead, Seek, Write};
 
 impl Image {
-
     /// Reads an image from a PNG file.
-    pub fn read_png<R: Read>(input: R) -> io::Result<Image> {
-        let decoder = png::Decoder::new(input);
-        let (info, mut reader) = decoder.read_info()?;
-        let pixel_format = match info.color_type {
-            png::ColorType::RGBA => PixelFormat::RGBA,
-            png::ColorType::RGB => PixelFormat::RGB,
+    pub fn read_png<R: BufRead + Seek>(input: R) -> io::Result<Image> {
+        let mut decoder = png::Decoder::new(input);
+        decoder.set_transformations(
+            png::Transformations::STRIP_16 | png::Transformations::EXPAND,
+        );
+        let info = decoder.read_header_info()?;
+        let (width, height) = (info.width, info.height);
+        let mut reader = decoder.read_info()?;
+
+        let (color_type, bit_depth) = reader.output_color_type();
+        assert!(bit_depth == png::BitDepth::Eight);
+        let pixel_format = match color_type {
+            png::ColorType::Rgba => PixelFormat::RGBA,
+            png::ColorType::Rgb => PixelFormat::RGB,
             png::ColorType::GrayscaleAlpha => PixelFormat::GrayAlpha,
             png::ColorType::Grayscale => PixelFormat::Gray,
-            _ => {
-                // TODO: Support other color types.
-                return Err(io::Error::new(io::ErrorKind::InvalidData,
-                                          format!("unsupported PNG color \
-                                                   type: {:?}",
-                                                  info.color_type)));
-            }
+            _ => unreachable!(), // EXPAND prevents paletted output
         };
-        if info.bit_depth != png::BitDepth::Eight {
-            // TODO: Support other bit depths.
-            return Err(io::Error::new(io::ErrorKind::InvalidData,
-                                      format!("unsupported PNG bit depth: \
-                                               {:?}",
-                                              info.bit_depth)));
 
-        }
-        let mut image = Image::new(pixel_format, info.width, info.height);
-        assert_eq!(image.data().len(), info.buffer_size());
+        let mut image = Image::new(pixel_format, width, height);
+        assert_eq!(Some(image.data().len()), reader.output_buffer_size());
         reader.next_frame(image.data_mut())?;
+        reader.finish()?;
         Ok(image)
     }
 
     /// Writes the image to a PNG file.
     pub fn write_png<W: Write>(&self, output: W) -> io::Result<()> {
         let color_type = match self.format {
-            PixelFormat::RGBA => png::ColorType::RGBA,
-            PixelFormat::RGB => png::ColorType::RGB,
+            PixelFormat::RGBA => png::ColorType::Rgba,
+            PixelFormat::RGB => png::ColorType::Rgb,
             PixelFormat::GrayAlpha => png::ColorType::GrayscaleAlpha,
             PixelFormat::Gray => png::ColorType::Grayscale,
             PixelFormat::Alpha => {
-                return self.convert_to(PixelFormat::GrayAlpha)
+                return self
+                    .convert_to(PixelFormat::GrayAlpha)
                     .write_png(output);
             }
         };
         let mut encoder = png::Encoder::new(output, self.width, self.height);
         encoder.set_color(color_type);
         encoder.set_depth(png::BitDepth::Eight);
+
         let mut writer = encoder.write_header()?;
-        writer.write_image_data(&self.data).map_err(|err| match err {
-            png::EncodingError::IoError(err) => err,
-            png::EncodingError::Format(msg) => {
-                io::Error::new(io::ErrorKind::InvalidData, msg.into_owned())
-            }
-        })
+        writer.write_image_data(&self.data)?;
+        Ok(())
     }
 }
